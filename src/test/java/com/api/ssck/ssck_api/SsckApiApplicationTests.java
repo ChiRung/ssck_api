483a0e4a3d2e45f4a840d780a4f63f16057eddd8
jinsung kim <rlawlstjd2404@gmail.com>
2023-02-06 13:32:32 +0900

    first commit

---
+++ src/test/java/com/api/ssck/ssck_api/SsckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ssck.ssck_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
